e710ffe3a46d57a365e83802c57cbc912c1104a4
zhangrenwei <zhangrenwei@hyperchain.cn>
2021-09-29 00:28:52 +0800

    爬楼梯、打家劫舍相关算法

--- src/main/java/cn/wahaha/test/dataStructure/dbPlan20210926/Solution.java
+++ src/main/java/cn/wahaha/test/dataStructure/dbPlan20210926/Solution.java
@@ -1,5 +1,7 @@
 package cn.wahaha.test.dataStructure.dbPlan20210926;
 
+import java.util.Map;
+
 public class Solution {
     // 斐波那契数列
     public int fib(int n) {
@@ -30,6 +32,89 @@ public class Solution {
         }
         return d;
     }
+
+    /**爬楼梯：
+     * 假设你正在爬楼梯。需要 n 阶你才能到达楼顶。
+     * 每次你可以爬 1 或 2 个台阶。你有多少种不同的方法可以爬到楼顶呢？
+     */
+    public int climbStairs(int n) {
+        if (n == 1) return 1;
+        if (n == 2) return 2;
+
+        int a = 1, b = 1, c = 2;
+        for (int i = 3; i <= n; i++) {
+            a = b;
+            b = c;
+            c = a + b;
+        }
+        return c;
+
+    }
+
+    /**使用最小花费爬楼梯：
+     *
+     * 数组的每个下标作为一个阶梯，第 i 个阶梯对应着一个非负数的体力花费值 cost[i]（下标从 0 开始）。
+     * 每当你爬上一个阶梯你都要花费对应的体力值，一旦支付了相应的体力值，你就可以选择向上爬一个阶梯或者爬两个阶梯。
+     * 请你找出达到楼层顶部的最低花费。在开始时，你可以选择从下标为 0 或 1 的元素作为初始阶梯。
+     */
+    public int minCostClimbingStairs(int[] cost) {
+        int n = cost.length;
+        int[] dp = new int[n + 1];
+        dp[0] = dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            dp[n] = Math.min(dp[n - 1] + cost[n - 1], dp[n - 2] + cost[n - 2]);
+        }
+        return dp[n];
+    }
+
+    /**
+     * 打家劫舍
+     * https://leetcode-cn.com/problems/house-robber/
+     */
+    public int rob(int[] nums) {
+        if (nums.length == 0) {
+            return -1;
+        }
+        int len = nums.length;
+        int[] dp = new int[len];
+        dp[0] = nums[0];
+
+        if (len >= 2) {
+            dp[1] = Math.max(nums[0], nums[1]);
+        }
+        for (int i = 2; i < len; i++) {
+            dp[i] = Math.max(dp[i-2] + nums[i], dp[i-1]);
+        }
+        return dp[len - 1];
+    }
+
+    /**
+     * 打家劫舍2
+     * https://leetcode-cn.com/problems/house-robber-ii/
+     */
+    public int rob_v2(int[] nums) {
+        if (nums.length == 0) {
+            return -1;
+        }
+        int len = nums.length;
+        if (len == 1) return nums[0];
+        if (len == 2) return Math.max(nums[0], nums[1]);
+
+        return Math.max(robRange(nums, 0, len - 2), robRange(nums, 1, len - 1));
+    }
+
+    public int robRange(int[] nums, int start, int end) {
+        int dpPre = nums[start], dpCurrent = Math.max(nums[start], nums[start + 1]);
+        for (int i = start + 2; i <= end; i++) {
+            int temp = dpCurrent;
+            dpCurrent = Math.max(dpPre + nums[i], dpCurrent);
+            dpPre = temp;
+        }
+        return dpCurrent;
+    }
+
+
+
 
 
 
